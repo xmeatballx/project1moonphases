d287ca07a8a6d101fc51998cf363bc004a2138b1
erikjensen5 <erik.jensen5@pcc.edu>
2020-01-29 14:34:08 -0800

    first commit texture glitch

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var moonsize=160;
+var moonPhase=0;
+var x=0;
+let moonTexture;
+
+function preload(){
+	moonTexture = loadImage ('moontexture.jpeg');
+}
+
+function setup(){
+	createCanvas(630,600,WEBGL);
+	background(0);
+	input = createInput();
+  input.position(20, 35);
+
+  button = createButton('submit');
+  button.position(input.x + input.width, 35);
+  button.mousePressed(phaseSet);
+	//noLoop();
+
+}
+
+function draw(){
+  drawMoon(moonPhase);
+		
+}
+
+function phaseSet(){
+	moonPhase = int(input.value());
+}
+
+function drawMoon(phase){
+	x=map((phase/10)+1.5,0,1,-1,1);
+	push();
+	ortho();
+	//ambientMaterial(250);
+	lightFalloff(1.0, 1.0, 1.0)
+		directionalLight(250, 250, 128, cos(x), 0, sin(x));
+		ambientLight(10,10,0);
+		noStroke();
+		texture(moonTexture);
+		sphere(moonsize);
+		pop();
+}
+
+
